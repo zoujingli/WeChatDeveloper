fc79380f0d368df8e17c769a3f75a28275561911
Anyon <zoujingli@qq.com>
2018-05-11 17:13:59 +0800

    [更新]修正测试参数

--- Test/config.php
+++ Test/config.php
@@ -19,7 +19,7 @@ return [
     'encodingaeskey' => 'BJIUzE0gqlWy0GxfPp4J1oPTBmOrNDIGPNav1YFH5Z5',
     // 配置商户支付参数
     'mch_id'         => "1332187001",
-    'mch_key'        => '11bd3d66d85f322a1e803cb587d18c3f',
+    'mch_key'        => 'A82DC5BD1F3359081049C568D8502BC5',
     // 配置商户支付双向证书目录
     'ssl_key'        => '',
     'ssl_cer'        => '',
