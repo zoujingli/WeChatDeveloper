60e032cdf60aa0ef31f28b2fa1704e5ee6c3a477
Anyon <zoujingli@qq.com>
2018-10-23 18:58:53 +0800

    [更新]修正企业打款到银行描述问题，增加单独p12证书支持

--- WeChat/Contracts/Tools.php
+++ WeChat/Contracts/Tools.php
@@ -29,7 +29,7 @@ class Tools
      * 缓存路径
      * @var null
      */
-    public static $cache_path = null;
+    public static $CachePath = null;
 
 
     /**
@@ -75,10 +75,8 @@ class Tools
         if (empty($mines)) {
             $content = file_get_contents('http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types');
             preg_match_all('#^([^\s]{2,}?)\s+(.+?)$#ism', $content, $matches, PREG_SET_ORDER);
-            foreach ($matches as $match) {
-                foreach (explode(" ", $match[2]) as $ext) {
-                    $mines[$ext] = $match[1];
-                }
+            foreach ($matches as $match) foreach (explode(" ", $match[2]) as $ext) {
+                $mines[$ext] = $match[1];
             }
             self::setCache('all_ext_mine', $mines);
         }
@@ -257,19 +255,33 @@ class Tools
     }
 
     /**
+     * 写入文件
+     * @param string $name 文件名称
+     * @param string $content 文件内容
+     * @return string
+     * @throws LocalCacheException
+     */
+    public static function pushFile($name, $content)
+    {
+        $file = self::getCacheName($name);
+        if (!file_put_contents($file, $content)) throw new LocalCacheException('local file write error.', '0');
+        return $file;
+    }
+
+    /**
      * 缓存配置与存储
      * @param string $name 缓存名称
      * @param string $value 缓存内容
      * @param int $expired 缓存时间(0表示永久缓存)
+     * @return string
      * @throws LocalCacheException
      */
     public static function setCache($name, $value = '', $expired = 3600)
     {
-        $cache_file = self::getCacheName($name);
+        $file = self::getCacheName($name);
         $content = serialize(['name' => $name, 'value' => $value, 'expired' => time() + intval($expired)]);
-        if (!file_put_contents($cache_file, $content)) {
-            throw new LocalCacheException('local cache error.', '0');
-        }
+        if (!file_put_contents($file, $content)) throw new LocalCacheException('local cache error.', '0');
+        return $file;
     }
 
     /**
@@ -279,8 +291,8 @@ class Tools
      */
     public static function getCache($name)
     {
-        $cache_file = self::getCacheName($name);
-        if (file_exists($cache_file) && ($content = file_get_contents($cache_file))) {
+        $file = self::getCacheName($name);
+        if (file_exists($file) && ($content = file_get_contents($file))) {
             $data = unserialize($content);
             if (isset($data['expired']) && (intval($data['expired']) === 0 || intval($data['expired']) >= time())) {
                 return $data['value'];
@@ -297,8 +309,8 @@ class Tools
      */
     public static function delCache($name)
     {
-        $cache_file = self::getCacheName($name);
-        return file_exists($cache_file) ? unlink($cache_file) : true;
+        $file = self::getCacheName($name);
+        return file_exists($file) ? unlink($file) : true;
     }
 
     /**
@@ -308,11 +320,11 @@ class Tools
      */
     private static function getCacheName($name)
     {
-        if (empty(self::$cache_path)) {
-            self::$cache_path = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'Cache' . DIRECTORY_SEPARATOR;
+        if (empty(self::$CachePath)) {
+            self::$CachePath = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'Cache' . DIRECTORY_SEPARATOR;
         }
-        self::$cache_path = rtrim(self::$cache_path, '/\\') . DIRECTORY_SEPARATOR;
-        file_exists(self::$cache_path) || mkdir(self::$cache_path, 0755, true);
-        return self::$cache_path . $name;
+        self::$CachePath = rtrim(self::$CachePath, '/\\') . DIRECTORY_SEPARATOR;
+        file_exists(self::$CachePath) || mkdir(self::$CachePath, 0755, true);
+        return self::$CachePath . $name;
     }
 }
