018918d342317f91b06978cef8d532ee16840623
Anyon <zoujingli@qq.com>
2018-12-05 16:58:37 +0800

    [更新]增加MyCurlFile文件类型

--- WeChat/Contracts/MyCurlFile.php
+++ WeChat/Contracts/MyCurlFile.php
@@ -9,24 +9,33 @@ namespace WeChat\Contracts;
  */
 class MyCurlFile extends \stdClass
 {
+    /**
+     * 当前数据类型
+     * @var string
+     */
+    public $datatype = 'MY_CURL_FILE';
 
     /**
      * MyCurlFile constructor.
-     * @param $filename
+     * @param string|array $filename
      * @param string $mimetype
      * @param string $postname
      * @throws \WeChat\Exceptions\LocalCacheException
      */
     public function __construct($filename, $mimetype = '', $postname = '')
     {
-
-        $this->mimetype = $mimetype;
-        $this->postname = $postname;
-        $this->content = base64_encode(file_get_contents($filename));
-        $this->extension = pathinfo($filename, PATHINFO_EXTENSION);
-        if (empty($this->extension)) $this->extension = 'tmp';
-        if (empty($this->mimetype)) $this->mimetype = Tools::getExtMine($this->extension);
-        if (empty($this->postname)) $this->postname = pathinfo($filename, PATHINFO_BASENAME);
+        if (is_array($filename)) {
+            foreach ($filename as $k => $v) $this->{$k} = $v;
+        } else {
+            $this->mimetype = $mimetype;
+            $this->postname = $postname;
+            $this->extension = pathinfo($filename, PATHINFO_EXTENSION);
+            if (empty($this->extension)) $this->extension = 'tmp';
+            if (empty($this->mimetype)) $this->mimetype = Tools::getExtMine($this->extension);
+            if (empty($this->postname)) $this->postname = pathinfo($filename, PATHINFO_BASENAME);
+            $this->content = base64_encode(file_get_contents($filename));
+            $this->tempname = md5($this->content) . ".{$this->extension}";
+        }
     }
 
     /**
@@ -36,7 +45,6 @@ class MyCurlFile extends \stdClass
      */
     public function get()
     {
-        $this->tempname = rand(100000, 999999) . ".{$this->extension}";
         $this->filename = Tools::pushFile($this->tempname, base64_decode($this->content));
         if (class_exists('CURLFile')) {
             return new \CURLFile($this->filename, $this->mimetype, $this->postname);
@@ -49,7 +57,7 @@ class MyCurlFile extends \stdClass
      */
     public function __destruct()
     {
-        Tools::delCache($this->tempname);
+        // Tools::delCache($this->tempname);
     }
 
 }
