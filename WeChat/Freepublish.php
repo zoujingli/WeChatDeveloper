7fec514e9eece91b41015df34241a78ef287b4a3
邹景立 <zoujingli@qq.com>
2022-09-05 13:27:36 +0800

    修改注释

--- WeChat/Freepublish.php
+++ WeChat/Freepublish.php
@@ -16,7 +16,7 @@ class Freepublish extends BasicWeChat
     /**
      * 发布接口
      * 开发者需要先将图文素材以草稿的形式保存（见“草稿箱/新建草稿”，如需从已保存的草稿中选择，见“草稿箱/获取草稿列表”）
-     * @param $media_id 选择要发布的草稿的media_id
+     * @param mixed $media_id 选择要发布的草稿的media_id
      * @return array
      * @throws Exceptions\LocalCacheException
      * @throws InvalidResponseException
@@ -30,11 +30,10 @@ class Freepublish extends BasicWeChat
 
     /**
      * 发布状态轮询接口
-     * @param string $media_id
-     * @param string $outType 返回处理函数
-     * @return array|string
-     * @throws Exceptions\LocalCacheException
-     * @throws InvalidResponseException
+     * @param mixed $publish_id
+     * @return array
+     * @throws \WeChat\Exceptions\InvalidResponseException
+     * @throws \WeChat\Exceptions\LocalCacheException
      */
     public function get($publish_id)
     {
@@ -46,7 +45,7 @@ class Freepublish extends BasicWeChat
     /**
      * 删除发布
      * 发布成功之后，随时可以通过该接口删除。此操作不可逆，请谨慎操作。
-     * @param $article_id 成功发布时返回的 article_id
+     * @param mixed $article_id 成功发布时返回的 article_id
      * @param int $index 要删除的文章在图文消息中的位置，第一篇编号为1，该字段不填或填0会删除全部文章
      * @return array
      * @throws Exceptions\LocalCacheException
@@ -60,10 +59,9 @@ class Freepublish extends BasicWeChat
         return $this->httpPostForJson($url, ['article_id' => $article_id, 'index' => $index]);
     }
 
-
     /**
      * 通过 article_id 获取已发布文章
-     * @param $article_id 要获取的草稿的article_id
+     * @param mixed $article_id 要获取的草稿的article_id
      * @return array
      * @throws Exceptions\LocalCacheException
      * @throws InvalidResponseException
@@ -74,7 +72,6 @@ class Freepublish extends BasicWeChat
         $this->registerApi($url, __FUNCTION__, func_get_args());
         return $this->httpPostForJson($url, ['article_id' => $article_id]);
     }
-
 
     /**
      * 获取成功发布列表
